f51e90a8729c0a47000fea5fbdc20d45cbbfa05e
rushabh95 <87568607+rushabh95@users.noreply.github.com>
2021-09-21 14:55:54 +0530

    Add files via upload
    a Python program to check whether a specified value is contained in a group of values.

---
+++ check boolean.py
@@ -0,0 +1,10 @@
+#Write a Python program to check whether a specified value is contained in a group of values.
+#Test Data :
+#  3 -> [1, 5, 8, 3] : True
+# -1 -> [1, 5, 8, 3] : False
+
+a = [1,5,8,3]
+print(3 in a)
+print()
+print(-1 in a)
+
