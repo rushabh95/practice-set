c1754a647a85d1eaa32abca2359caa4cdf9af9f3
rushabh95 <87568607+rushabh95@users.noreply.github.com>
2021-10-02 17:34:40 +0530

    Add files via upload
    Python program to find the index of an item in a specified list.

---
+++ 8.py
@@ -0,0 +1,5 @@
+#Write a Python program to find the index of an item in a specified list.
+
+x = ['apple','banana', "grape", 'peach', 'kite' , 'ball', 'pan']
+
+print("the index of grape is",x.index("grape"))
